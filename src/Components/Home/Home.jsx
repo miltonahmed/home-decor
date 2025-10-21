0c8b48d9379762c67b7e298bcb17a39be8cf8e23
Milton Ahmed <miltonahmed08@yahoo.com>
2025-10-21 04:03:49 +0400

    I had 16 product data entries, and I took (or removed) 6 from them.

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,16 +1,20 @@
+import { Suspense } from "react";
 import { useLoaderData } from "react-router";
 import ProductCard from "./ProductCard";
 
 const Home = () => {
   const products = useLoaderData()
   console.log(products)
+  const featuredProducts = products.slice(0, 6)
   return (
     <div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-        {products.map(product => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </div>
+      <Suspense fallback='loading...........'>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+          {featuredProducts.map(product => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      </Suspense>
     </div>
   );
 }
