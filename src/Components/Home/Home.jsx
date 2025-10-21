f36ceb4dfb0b6b89bc121f4a61652cb50fd3b386
Milton Ahmed <miltonahmed08@yahoo.com>
2025-10-21 05:09:22 +0400

    Make a Custom hooks appply ProductCard Section

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,14 +1,28 @@
 import { Suspense } from "react";
-import { useLoaderData } from "react-router";
+import { Link } from "react-router";
+import { useProducts } from "../../Hooks/useProducts";
 import ProductCard from "./ProductCard";
 
 const Home = () => {
-  const products = useLoaderData()
-  console.log(products)
+  // const products = useLoaderData()
+  const { products, loading, error } = useProducts()
+  // cut 6 product to 16 
   const featuredProducts = products.slice(0, 6)
   return (
     <div>
-      <Suspense fallback='loading...........'>
+      <div className="flex justify-between items-center mb-6">
+        <h1 className="text-2xl font-bold">Featured Products</h1>
+        <Link to="/products" className="text-blue-500 hover:underline">
+          See All Products
+        </Link>
+      </div>
+      
+
+      <Suspense
+        fallback={
+          <div className="text-center py-10">Loading featured products...</div>
+        }
+      >
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
           {featuredProducts.map(product => (
             <ProductCard key={product.id} product={product} />
